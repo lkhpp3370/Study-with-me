9ef7bb223fd54dd4c66d5a8285c4c0fb347c7dc2
rlagustjdla <145083341+rlagustjdla@users.noreply.github.com>
2025-10-29 22:17:10 +0900

    Add files via upload

---
+++ front/app.config.js
@@ -0,0 +1,39 @@
+export default {
+  expo: {
+    name: 'study',
+    slug: 'study',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      softwareKeyboardLayoutMode: 'resize' ,
+      edgeToEdgeEnabled: true,
+      package: 'com.anonymous.study',
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    extra: {
+      BACKEND_URL: process.env.BACKEND_URL,
+      KAKAO_JS_KEY: process.env.KAKAO_JS_KEY,
+      eas: {
+        projectId: 'e3e5f5d5-84d5-4d5a-9596-ab2cdaa62ec1',
+      }
+    },
+  },
+};
+
